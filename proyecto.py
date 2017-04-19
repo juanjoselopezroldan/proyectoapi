3255809f25a031b3549c93da1ed9485b7835ba54
Juan Jose <jjlr1994@gmail.com>
2017-04-19 18:51:28 +0200

    modificacion de ficheros

--- proyecto.py
+++ proyecto.py
@@ -13,15 +13,14 @@ def inicio():
 def resultado():
 	key="AIzaSyBFRrsWet-kt-RxnkWVpUdeZ7ep4s1hdNc"
 	sit = request.forms.get('sitio')
-    rad = request.forms.get('radio')
-    
-    urlbase="http://maps.googleapis.com/maps/api/"
-    payload={"address":sit,"sensor":"false"}
-    r=requests.get(urlbase+"geocode/json",params=payload)
-    print r
-    print r.status_code
-    if r.status_code == 200:
-    	js=json.loads(r.text)
+	rad = request.forms.get('radio')
+	urlbase="http://maps.googleapis.com/maps/api/"
+	payload={"address":sit,"sensor":"false"}
+	r=requests.get(urlbase+"geocode/json",params=payload)
+	print r
+	print r.status_code
+	if r.status_code == 200:
+		js=json.loads(r.text)
     
 @route('/static/<filepath:path>')
 def server_static(filepath):
