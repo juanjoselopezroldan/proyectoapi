ba2c78964baf8b0a3ebc9eb819d035c2bb48a635
Juan Jose <jjlr1994@gmail.com>
2017-04-19 17:48:46 +0200

    mod de proyecto

--- proyecto.py
+++ proyecto.py
@@ -1,5 +1,20 @@
 import bottle
 from bottle import Bottle,route,run,request,template,static_file
 import json
+import os
 from sys import argv
 import requests
+
+@route('/',method="get")
+def inicio():
+	    return template ('template.tpl')
+@route('/map',method="post")
+def resultado():
+
+
+@route('/static/<filepath:path>')
+def server_static(filepath):
+	return static_file(filepath, root='static')
+
+if __name__ == '__main__':
+	run(host='0.0.0.0',port=argv[1])
