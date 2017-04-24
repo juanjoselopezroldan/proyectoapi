15254a935ce2f5bed0ff64b56e2f2e3f740d92f4
Juan Jose <jjlr1994@gmail.com>
2017-04-24 14:05:20 +0200

    modifiacion del fichero

--- proyecto.py
+++ proyecto.py
@@ -25,7 +25,7 @@ def resultado():
 			lng=i["geometry"]["location"]["lng"]
 		lat_long=str(lat)+","+str(lng)
 		payload2={"location":lat_long,"language":"es","radius":rad,"types":lug,"keyword":"cruise","sensor":"false","key":key,"libraries":"places"}
-		r2=requests.get(urlbase+"place/nearbysearch/json",params=payload2)
+		r2=requests.post(urlbase+"place/nearbysearch/json",params=payload2)
 		cont=1
 		cont2=[1]
 		nombres=[]
