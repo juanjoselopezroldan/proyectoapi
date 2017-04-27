85a975ea9377e3f966b8435a10b27b65ab691df8
Juan Jose <jjlr1994@gmail.com>
2017-04-27 17:07:29 +0200

    modifiacion del fichero

--- proyecto.py
+++ proyecto.py
@@ -19,37 +19,6 @@ def resultado(token=''):
 	urlbase="https://maps.googleapis.com/maps/api/"
 	payload={"address":sit,"sensor":"false"}
 	r=requests.get(urlbase+"geocode/json",params=payload)
-		if token=='token':
-		pagina = request.forms.get('next')
-		if r.status_code == 200:
-		js=json.loads(r.text)
-		for i in js["results"]:
-			lat=i["geometry"]["location"]["lat"]
-			lng=i["geometry"]["location"]["lng"]
-		lat_long=str(lat)+","+str(lng)
-		payload2={"location":lat_long,"language":"es","radius":rad,"query":lug,"keyword":"cruise","sensor":"false","key":key}
-		r2=requests.post(urlbase+"place/textsearch/json",params=payload2)
-		cont=1
-		cont2=[1]
-		nombres=[]
-		calles=[]
-		latitud=[]
-		longitud=[]
-		siguiente="nada"
-		if r2.status_code==200:
-			js2=json.loads(r2.text)
-			if js2.has_key("next_page_token"):
-				siguiente=js2["next_page_token"]
-			for i2 in js2["results"]:
-				cont=cont+1
-				cont2.append(cont)
-				nombres.append(i2["name"])
-				calles.append(i2["formatted_address"])
-				latitud.append(i2["geometry"]["location"]["lat"])
-				longitud.append(i2["geometry"]["location"]["lng"])
-			cont=cont-1
-		return template('template2.tpl',  siguiente=siguiente, js2=js2, lat=lat, lng=lng, latitud=latitud, longitud=longitud, nombre=nombres, calle=calles, cont=cont, cont2=cont2)
-
 	if r.status_code == 200:
 		js=json.loads(r.text)
 		for i in js["results"]:
