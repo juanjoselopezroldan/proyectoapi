c4bb5cb56cac2cd0b585ba1b7b0596636797624a
Juan Jose <jjlr1994@gmail.com>
2017-04-27 16:40:18 +0200

    modifiacion del fichero

--- proyecto.py
+++ proyecto.py
@@ -13,6 +13,45 @@ def inicio():
 @route('/map/<token>',method="post")
 def resultado(token=''):
 	key=os.environ['key']
+	if token=='token':
+		pagina = request.forms.get('next')
+		sit = request.forms.get('sitio')
+		lug = request.forms.get('lugar')
+		rad = request.forms.get('radio')
+		urlbase="https://maps.googleapis.com/maps/api/"
+		payload={"address":sit,"sensor":"false"}
+		r=requests.get(urlbase+"geocode/json",params=payload)
+		if r.status_code == 200:
+			js=json.loads(r.text)
+			for i in js["results"]:
+				lat=i["geometry"]["location"]["lat"]
+				lng=i["geometry"]["location"]["lng"]
+			lat_long=str(lat)+","+str(lng)
+			payload2={"location":lat_long,"language":"es","radius":rad,"query":lug,"keyword":"cruise","sensor":"false","key":key}
+			r2=requests.post(urlbase+"place/textsearch/json",params=payload2)
+			cont=1
+			cont2=[1]
+			nombres=[]
+			calles=[]
+			latitud=[]
+			longitud=[]
+			siguiente="nada"
+			if r2.status_code==200:
+				js2=json.loads(r2.text)
+				if js2.has_key("next_page_token"):
+					siguiente=js2["next_page_token"]
+				for i2 in js2["results"]:
+					cont=cont+1
+					cont2.append(cont)
+					nombres.append(i2["name"])
+					calles.append(i2["formatted_address"])
+					latitud.append(i2["geometry"]["location"]["lat"])
+					longitud.append(i2["geometry"]["location"]["lng"])
+				cont=cont-1
+			payload2={"location":lat_long,"language":"es","radius":rad,"query":lug,"keyword":"cruise","sensor":"false","key":key}
+			r2=requests.post(urlbase+"place/textsearch/json",params=payload2)
+			return template('template2.tpl',  siguiente=siguiente, js2=js2, lat=lat, lng=lng, latitud=latitud, longitud=longitud, nombre=nombres, calle=calles, cont=cont, cont2=cont2)
+
 	sit = request.forms.get('sitio')
 	lug = request.forms.get('lugar')
 	rad = request.forms.get('radio')
@@ -46,11 +85,6 @@ def resultado(token=''):
 				latitud.append(i2["geometry"]["location"]["lat"])
 				longitud.append(i2["geometry"]["location"]["lng"])
 			cont=cont-1
-		return template('template2.tpl',  siguiente=siguiente, js2=js2, lat=lat, lng=lng, latitud=latitud, longitud=longitud, nombre=nombres, calle=calles, cont=cont, cont2=cont2)
-	if token=='token':
-		pagina = request.forms.get('next')
-		payload2={"location":lat_long,"language":"es","radius":rad,"query":lug,"keyword":"cruise","sensor":"false","key":key}
-		r2=requests.post(urlbase+"place/textsearch/json",params=payload2)
 		return template('template2.tpl',  siguiente=siguiente, js2=js2, lat=lat, lng=lng, latitud=latitud, longitud=longitud, nombre=nombres, calle=calles, cont=cont, cont2=cont2)
 
 @route('/static/<filepath:path>')
