884bbca080466109ffb4b08eee82ac1e554664dc
Juan Jose <jjlr1994@gmail.com>
2017-04-29 14:50:07 +0200

    modifiacion del fichero

--- proyecto.py
+++ proyecto.py
@@ -27,60 +27,60 @@ def resultado():
 			lng=i["geometry"]["location"]["lng"]
 		lat_long=str(lat)+","+str(lng)
 
-	if request.method=="post":
-		prueba="segundo"
-		key=os.environ['key']
-		token=request.forms.get("next")
-		sit = request.forms.get('sitio')
-		rad = request.forms.get('radio')
-		lug = request.forms.get('lugar')
-		payload2={"key":key,"next_page_token":token}
-		r2=requests.post(urlbase+"place/textsearch/json",params=payload2)
-		cont=1
-		cont2=[1]
-		nombres=[]
-		calles=[]
-		latitud=[]
-		longitud=[]
-		siguiente="nada"
-		if r2.status_code==200:
-			js2=json.loads(r2.text)
-			if js2.has_key("next_page_token"):
-				siguiente=js2["next_page_token"]
-			for i2 in js2["results"]:
-				cont=cont+1
-				cont2.append(cont)
-				nombres.append(i2["name"])
-				calles.append(i2["formatted_address"])
-				latitud.append(i2["geometry"]["location"]["lat"])
-				longitud.append(i2["geometry"]["location"]["lng"])
-			cont=cont-1
-		return template('template2.tpl', prueba=prueba,  siguiente=siguiente, js2=js2, rad=rad, lug=lug, latitud=latitud, longitud=longitud, nombre=nombres, calle=calles, cont=cont, cont2=cont2, lat_long=lat_long, clave=key)
+		if request.method=="post":
+			prueba="segundo"
+			key=os.environ['key']
+			token=request.forms.get("next")
+			sit = request.forms.get('sitio')
+			rad = request.forms.get('radio')
+			lug = request.forms.get('lugar')
+			payload2={"key":key,"next_page_token":token}
+			r2=requests.get(urlbase+"place/textsearch/json",params=payload2)
+			cont=1
+			cont2=[1]
+			nombres=[]
+			calles=[]
+			latitud=[]
+			longitud=[]
+			siguiente="nada"
+			if r2.status_code==200:
+				js2=json.loads(r2.text)
+				if js2.has_key("next_page_token"):
+					siguiente=js2["next_page_token"]
+				for i2 in js2["results"]:
+					cont=cont+1
+					cont2.append(cont)
+					nombres.append(i2["name"])
+					calles.append(i2["formatted_address"])
+					latitud.append(i2["geometry"]["location"]["lat"])
+					longitud.append(i2["geometry"]["location"]["lng"])
+				cont=cont-1
+			return template('template2.tpl', prueba=prueba,  siguiente=siguiente, js2=js2, rad=rad, lug=lug, latitud=latitud, longitud=longitud, nombre=nombres, calle=calles, cont=cont, cont2=cont2, lat_long=lat_long, clave=key)
 
-	else:
-		prueba="primero"
-		payload2={"location":lat_long,"language":"es","radius":rad,"query":lug,"keyword":"cruise","sensor":"false","key":key}
-		r2=requests.post(urlbase+"place/textsearch/json",params=payload2)
-		cont=1
-		cont2=[1]
-		nombres=[]
-		calles=[]
-		latitud=[]
-		longitud=[]
-		siguiente="nada"
-		if r2.status_code==200:
-			js2=json.loads(r2.text)
-			if js2.has_key("next_page_token"):
-				siguiente=js2["next_page_token"]
-			for i2 in js2["results"]:
-				cont=cont+1
-				cont2.append(cont)
-				nombres.append(i2["name"])
-				calles.append(i2["formatted_address"])
-				latitud.append(i2["geometry"]["location"]["lat"])
-				longitud.append(i2["geometry"]["location"]["lng"])
-			cont=cont-1
-		return template('template2.tpl', prueba=prueba ,siguiente=siguiente, js2=js2, rad=rad, lug=lug, latitud=latitud, longitud=longitud, nombre=nombres, calle=calles, cont=cont, cont2=cont2, lat_long=lat_long, clave=key)
+		else:
+			prueba="primero"
+			payload2={"location":lat_long,"language":"es","radius":rad,"query":lug,"keyword":"cruise","sensor":"false","key":key}
+			r2=requests.get(urlbase+"place/textsearch/json",params=payload2)
+			cont=1
+			cont2=[1]
+			nombres=[]
+			calles=[]
+			latitud=[]
+			longitud=[]
+			siguiente="nada"
+			if r2.status_code==200:
+				js2=json.loads(r2.text)
+				if js2.has_key("next_page_token"):
+					siguiente=js2["next_page_token"]
+				for i2 in js2["results"]:
+					cont=cont+1
+					cont2.append(cont)
+					nombres.append(i2["name"])
+					calles.append(i2["formatted_address"])
+					latitud.append(i2["geometry"]["location"]["lat"])
+					longitud.append(i2["geometry"]["location"]["lng"])
+				cont=cont-1
+			return template('template2.tpl', prueba=prueba ,siguiente=siguiente, js2=js2, rad=rad, lug=lug, latitud=latitud, longitud=longitud, nombre=nombres, calle=calles, cont=cont, cont2=cont2, lat_long=lat_long, clave=key)		
 
 @route('/static/<filepath:path>')
 def server_static(filepath):
